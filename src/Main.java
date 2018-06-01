e6eda9638de456464f3d981ea5b14fb69ff12f7e
Ivana <ivana.vujic.milic@gmail.com>
2018-06-01 11:44:17 +0200

    Added testLoops

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,35 @@
 
+import loops.Loops;
+
+
 public class Main {
 
     
     public static void main(String[] args) {
     
-         Car myCar = new Car();
-         myCar.printAtributes();
-         myCar.changeLicense("av123");
-         myCar.maxDistance();
-         double distance = myCar.maxDistance();
-         System.out.println("Maksimalna distanca je: " + distance);
-         System.out.println();
-         
-         Car audi = new Car("Audi", 50, 2);
-         audi.printAtributes();
-         audi.changeLicense("kl6475");
-         audi.changeWeight(1500);
-         System.out.println("Maksimalna distanca je: " + audi.maxDistance());
-         System.out.println();
-         
-         audi.fuelUp();
-         audi.printAtributes();
-         audi.travel(20);
-         audi.printAtributes();
-         audi.travel(30);
-         audi.printAtributes();
-        
-         Car noviAuto = new Car("noviAuto", 40, 3, 5, 1);
+//         Car myCar = new Car();
+//         myCar.printAtributes();
+//         myCar.changeLicense("av123");
+//         myCar.maxDistance();
+//         double distance = myCar.maxDistance();
+//         System.out.println("Maksimalna distanca je: " + distance);
+//         System.out.println();
+//         
+//         Car audi = new Car("Audi", 50, 2);
+//         audi.printAtributes();
+//         audi.changeLicense("kl6475");
+//         audi.changeWeight(1500);
+//         System.out.println("Maksimalna distanca je: " + audi.maxDistance());
+//         System.out.println();
+//         
+//         audi.fuelUp();
+//         audi.printAtributes();
+//         audi.travel(20);
+//         audi.printAtributes();
+//         audi.travel(30);
+//         audi.printAtributes();
+//        
+//         Car noviAuto = new Car("noviAuto", 40, 3, 5, 1);
 //         noviAuto.printAtributes();
 //         noviAuto.printPassengersInfo();
 //         noviAuto.getIn();
@@ -45,14 +48,41 @@ public class Main {
 //         noviAuto.getOut();
 //         noviAuto.getOut();
 //         
-         noviAuto.printPassengersInfo();
+//         noviAuto.printPassengersInfo();
 
-         noviAuto.getIn(2);
 //         noviAuto.getIn(2);
+////         noviAuto.getIn(2);
          
          
-         noviAuto.getOut(1);
-         noviAuto.getOut(3);
+//         noviAuto.getOut(1);
+//         noviAuto.getOut(3);
+
+//            int x = 5; 
+//            while(x > 0){
+//                System.out.println(x);
+//                   x = x - 1;
+////                   x--;
+//            }
+//
+//
+//            do{
+//                System.out.println(x);
+//                x--;
+//            }while(x > 0);
+            
+            
+//            for(int i = 7; i <= 15; i++){
+//                System.out.println(i);
+//            }
+            
+//            Loops loop1 = new Loops();
+//              loop1.testWhileLoop();
+//              loop1.testForLoop();
+            
+//                Loops.testWhileLoop();
+//                Loops.testForLoop();
+                Loops.testDoubleForLoop();
+            
     }
     
     }
---
+++ src/loops/Loops.java
@@ -0,0 +1,30 @@
+
+package loops;
+
+public class Loops {
+     
+    public static void testWhileLoop(){
+        int x = 3;
+        while(x > -3){
+            System.out.println("Value of x: " + x);
+        }
+    }
+    
+    public static void testForLoop(){
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+    
+    
+    public static void testDoubleForLoop(){
+        for (int i = 0; i < 10; i++) {
+            
+            for (int j = 0; j < 10; j++) {
+                System.out.println("i = " + i + ", j + " + j);
+            }
+        }
+    }
+   
+    
+}
